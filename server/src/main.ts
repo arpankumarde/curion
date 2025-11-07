de7c25515f3e630dcc5a0f8cd26a7bdca11413d9
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-11-07 23:33:35 +0530

    feat: added swagger documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder().setTitle('Curion').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
